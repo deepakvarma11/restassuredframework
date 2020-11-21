ba0dd5deb913f296fc96d853d0380bfa3cb10e8b
Deepak varma <deepakvarma543@gmail.com>
2020-11-21 21:25:53 +0530

    Added the JsonCompare for testCase GetBooks

--- src/test/java/scripts/GetBooks.java
+++ src/test/java/scripts/GetBooks.java
@@ -1,22 +1,81 @@
 package scripts;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
+import Utils.ExtentTestManager;
 import commonlib.TestBase;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
-public class GetBooks extends TestBase{
+public class GetBooks extends TestBase {
 
 	@Test
 	public void getBooks() {
 		Response res = av.getMethod(userID, "getMethodNoHeader", "GetBooks");
-//		res.getBody().prettyPrint();
-		JsonPath json = res.jsonPath();
-		
+
+		int statusCode = HttpStatus.SC_OK;
+		String methodName = new Exception().getStackTrace()[0].getMethodName();
+
+		JSONObject ExpectedResponse = new JSONObject();
+		ExpectedResponse.put("isbn", "9781449325862");
+		ExpectedResponse.put("title", "Git Pocket Guide");
+
+		String jsonString1 = res.asString();
+		List<Map<String, String>> booksOfUser1 = JsonPath.from(jsonString1).get("books");
+		JSONObject ActualResponseObject = new JSONObject(booksOfUser1.get(0));
+
+		System.out.println("firsobject " + ActualResponseObject.toString());
+
+		ActualResponseObject.remove("subTitle");
+		ActualResponseObject.remove("author");
+		ActualResponseObject.remove("publish_date");
+		ActualResponseObject.remove("publisher");
+		ActualResponseObject.remove("pages");
+		ActualResponseObject.remove("description");
+		ActualResponseObject.remove("website");
+
+		System.out.println("After removing " + ActualResponseObject.toString());
+
+		JSONCompareResult result = JSONCompare.compareJSON(ExpectedResponse, ActualResponseObject,
+				JSONCompareMode.STRICT);
+
+		boolean compareResult = false;
+
+		System.out.println(result.passed());
+		if (result.passed() == true) {
+			compareResult = true;
+		}
+
+		ExtentTestManager
+				.logsGeneration("Actual response and Expected response are same and result is " + compareResult);
+
+		String jsonString = res.asString();
+		List<Map<String, String>> booksOfUser = JsonPath.from(jsonString).get("books");
+//		System.out.println(booksOfUser.get(0));
+		Assert.assertNotEquals(0, booksOfUser.size());
+
+		if (statusCode == res.statusCode() && booksOfUser.size() != 0 && compareResult == true) {
+			ExtentTestManager.getTest().log(Status.PASS, methodName + " GetBooks Passed Expected Status " + statusCode
+					+ " and Actual Status : " + res.getStatusCode());
+		} else {
+			ExtentTestManager.getTest().log(Status.FAIL,
+					methodName + " GetBooks Passed Expected Status " + statusCode + " and Actual Status : "
+							+ res.getStatusCode() + " and the Response is : <br />" + res.prettyPrint() + "<br />");
+		}
+
 // we can use this for POST method		System.out.println(json.getString("books.isbn").contains("9781449331818") +" Pass");
-		
-		
+
 	}
 
 }
--- src/test/java/scripts/TokenGenerationTestCase.java
+++ src/test/java/scripts/TokenGenerationTestCase.java
@@ -3,55 +3,83 @@ package scripts;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.Assert;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import Utils.TestListener;
+import com.aventstack.extentreports.Status;
+
+import Utils.ExtentTestManager;
 import commonlib.TestBase;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class TokenGenerationTestCase extends TestBase {
 
-	@Test
+	@Test(priority=0)
 	void getTokenPositive() {
 
-		
 		JSONObject requestBody = new JSONObject(read.getCellDataValue("APITestData", "Body", "Token"));
 
 		Response res = av.postMethod(requestBody, "postMethodNoHeaders", "Token");
 
 		JsonPath responseJson = res.jsonPath();
-		
+
 		System.out.println(res.prettyPrint());
 
 		Assert.assertEquals(HttpStatus.SC_OK, res.getStatusCode());
 		Assert.assertTrue(res.asString().contains("token"));
 
 		token = responseJson.getString("token");
-//		System.out.println(token);
-		
-		
+
+		Boolean contains = res.asString().contains("token");
+		int statusCode = HttpStatus.SC_OK;
+		String methodName = new Exception().getStackTrace()[0].getMethodName();
+
+		if (statusCode == res.getStatusCode() && contains == true) {
+			ExtentTestManager.getTest().log(Status.PASS,
+					methodName + " Positive Scenario Passed Expected Status " + statusCode + " and Actual Status : "
+							+ res.getStatusCode() + " And Result is: " + responseJson.getString("result"));
+		} else {
+			ExtentTestManager.getTest().log(Status.FAIL,
+					methodName + " Positive Scenario Failed Expected Status " + statusCode + " and Actual Status : "
+							+ res.getStatusCode() + " and the Response is : <br />" + res.prettyPrint() + "<br />");
+		}
 
 	}
-	
-	@Test
+
+	@Test(priority=1)
 	public void getTokenNegative() {
 		JSONObject requestBody = new JSONObject(read.getCellDataValue("APITestData", "Body", "Token"));
-		
+
 		requestBody.put("password", "Deepak@11");
 
 		Response res = av.postMethod(requestBody, "postMethodNoHeaders", "Token");
 
 		JsonPath responseJson = res.jsonPath();
-		
+
 		System.out.println(res.prettyPrint());
 
 		Assert.assertEquals(HttpStatus.SC_OK, res.getStatusCode());
 		Assert.assertTrue(res.asString().contains("token"));
 
-		Assert.assertEquals(responseJson.getString("status"),"Failed");
-		
-		System.out.println("Result "+ responseJson.getString("result"));
+		Assert.assertEquals(responseJson.getString("status"), "Failed");
+
+//		System.out.println("Result " + responseJson.getString("result"));
+
+		String statusInResponse = responseJson.getString("status");
+
+		int statusCode = HttpStatus.SC_OK;
+		String responseStatus = "Failed";
+
+		String methodName = new Exception().getStackTrace()[0].getMethodName();
+
+		if (responseStatus.equalsIgnoreCase(statusInResponse)) {
+			ExtentTestManager.getTest().log(Status.PASS,
+					methodName + " Negative Scenario Passed Expected Status " + statusCode + " and Actual Status : "
+							+ res.getStatusCode() + "  And Result is: " + responseJson.getString("result"));
+		} else if (statusCode != res.getStatusCode()) {
+			ExtentTestManager.getTest().log(Status.FAIL,
+					methodName + " Negative Scenario Failed Expected Status " + statusCode + " and Actual Status : "
+							+ res.getStatusCode() + " and the Response is : <br />" + res.prettyPrint() + "<br />");
+		}
 	}
 }
